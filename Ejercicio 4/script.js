3da71529567b5a2e6956df9a85535ead89e9fb57
Angel gabriel tula <tulaangel34521@gmail.com>
2024-05-28 21:37:38 -0300

    Ejercicio 4 hecho

---
+++ Ejercicio 4/script.js
@@ -0,0 +1,30 @@
+const formulario = $("formulario");
+    const mensaje = $("mensaje")
+
+    formulario.addEventListener("submit",(e) => {
+        const peso = (formulario["peso"].value);
+        const altura = parseFloat(formulario["altura"].value)
+        const imc = peso/altura**2
+        alert(imc)
+        e.preventDefault();
+    });
+
+    formulario.addEventListener("focusout",() => {
+        mensaje.style.color = "red"
+        if (formulario["peso"].value === "") {
+            mensaje.textContent = "Peso vacio - Ingrese su peso..";   
+        }  else if (formulario["peso"].value <= 0) {
+            mensaje.textContent = "Su peso no puede ser negativo o cero.";
+        }  else if (formulario["altura"].value === "") {
+            mensaje.textContent = "Altura vacia - Ingrese su altura..";
+        }  else if(formulario["altura"].value <= 0) {
+            mensaje.textContent = "Su altura no puede ser negativo o cero.";
+        }   else {
+            mensaje.textContent = "Todos los datos son correctos!!";
+            mensaje.style.color = "green";
+        }
+    });
+
+    function $(elemento) {
+        return document.getElementById(elemento);
+    };
